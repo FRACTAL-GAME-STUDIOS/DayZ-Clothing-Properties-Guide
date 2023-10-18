0cac0b7bfbcda0f2237cbf524c101dcae71dd1b5
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2023-10-18 22:23:03 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# DayZ-Clothing-Properties-Guide
- This guide is primarily aimed at providing detailed information about available properties in DayZ's config files for modders. 
+
+# DayZ Clothing Properties Guide
+
+Welcome to the `DayZ-Clothing-Properties-Guide` repository. This guide is primarily aimed at providing detailed information about available properties related to clothing in DayZ's config files for modders. It is intended to serve as a quick reference for those interested in modding and customizing DayZ's clothing items.
+
+## Repository Structure
+
+- `/docs`: Contains the main guides and documentation.
+  - `properties_guide.cpp`: Detailed guide about properties used in DayZ's clothing config files.
+
+## Contribution
+
+We welcome contributions from the community! If you have information on any of the properties, especially those marked as "Still investigating", or if you want to add more details, examples, or any other relevant information, please feel free to make a pull request.
+
+To contribute:
+1. Fork this repository.
+2. Make your changes or additions.
+3. Create a pull request with a description of your changes.
+
+We appreciate any help in making this guide more comprehensive and useful for the DayZ modding community!
---
+++ properties_comments.cpp
@@ -0,0 +1,812 @@
+// Still investigating
+AttType = <value>;
+
+// Still investigating
+BoneNames[] = <value>;
+
+// Still investigating
+ContinuouActions[] = <value>;
+
+// Still investigating
+DirFilterSpeed = <value>;
+
+// Still investigating
+DirFilterTimeout = <value>;
+
+// Still investigating
+HeadDirFilterSpeed = <value>;
+
+// Still investigating
+HeadDirFilterTimeout = <value>;
+
+// Still investigating
+Icon = <value>;
+
+// Still investigating
+MaxSprintAngle = <value>;
+
+// Still investigating
+NVOptic = <value>;
+
+// Still investigating
+P3DProxies[] = <value>;
+
+// Still investigating
+RefTexsMats[] = <value>;
+
+// Still investigating
+RunSpringMaxChange = <value>;
+
+// Still investigating
+RunSpringTimeout = <value>;
+
+// Still investigating
+VoNRanges[] = <value>;
+
+// Item's ability to absorb moisture.
+absorbency = 0.7;
+
+// Accuracy of the item, typically related to weapons.
+accuracy = 1.0;
+
+// Still investigating
+action = <value>;
+
+// Still investigating
+actionSounds = <value>;
+
+// Still investigating
+afMax = <value>;
+
+// Still investigating
+aiAgentTemplate = <value>;
+
+// Still investigating
+aimMaxDistance = <value>;
+
+// Still investigating
+aimMinDistance = <value>;
+
+// Still investigating
+aimSpeed = <value>;
+
+// Still investigating
+allowOnAnimal = <value>;
+
+// Still investigating
+allowOnInfected = <value>;
+
+// Still investigating
+allowOnPlayer = <value>;
+
+// Still investigating
+allowOwnedCargoManipulation = <value>;
+
+// Amount of ammunition the item can hold.
+ammo = 30;
+
+// Type of ammunition the item uses (if it's a weapon).
+ammoType = "9mm";
+
+// Still investigating
+angle = <value>;
+
+// Still investigating
+angleInnerRatio = <value>;
+
+// Still investigating
+anim = <value>;
+
+// Still investigating
+animClass = <value>;
+
+// Still investigating
+animEventIds[] = <value>;
+
+// Still investigating
+animGraph = <value>;
+
+// Still investigating
+animPeriod = <value>;
+
+// Still investigating
+animalBonePairs[] = <value>;
+
+// Indicates if the item has associated animations.
+animated = 1;
+
+// Still investigating
+attachmentAction = <value>;
+
+// Still investigating
+attachments[] = <value>;
+
+// Still investigating
+attackName = <value>;
+
+// Still investigating
+attackSounds = <value>;
+
+// Still investigating
+attackWidth = <value>;
+
+// Still investigating
+autoSwitchOffWhenInCargo = <value>;
+
+// Still investigating
+autocenter = <value>;
+
+// Still investigating
+biological = <value>;
+
+// Brightness of the item, typically related to light sources.
+brightness = 0.8;
+
+// Still investigating
+cameraDir = <value>;
+
+// Still investigating
+canBeDigged = <value>;
+
+// Still investigating
+canCarryBackPack = <value>;
+
+// Still investigating
+change = <value>;
+
+// Still investigating
+chaseSounds = <value>;
+
+// Still investigating
+chemical = <value>;
+
+// Still investigating
+clothingType = <value>;
+
+// Color of the item.
+color = "blue";
+
+// Still investigating
+color[] = <value>;
+
+// Still investigating
+componentNames[] = <value>;
+
+// Cooldown time after an action with the item is performed.
+cooldown = 5;
+
+// Still investigating
+count = <value>;
+
+// Still investigating
+crosshairWidth = <value>;
+
+// Still investigating
+customHeads[] = <value>;
+
+// Amount of damage the item can inflict or receive.
+damage = 0.5;
+
+// Still investigating
+dayLight = <value>;
+
+// Still investigating
+debugDraw = <value>;
+
+// Still investigating
+debug_ItemCategory = <value>;
+
+// Still investigating
+decayedTexture = <value>;
+
+// Still investigating
+defaultInstance = <value>;
+
+// Still investigating
+defaultinstance = <value>;
+
+// Short description of the item.
+descriptionShort = "Una camiseta azul hecha de algod�n.";
+
+// Still investigating
+direction = <value>;
+
+// Still investigating
+directories[] = <value>;
+
+// Display name of the item in the game.
+displayName = "Camiseta Azul";
+
+// Still investigating
+displayWeight = <value>;
+
+// Still investigating
+displayname = <value>;
+
+// Still investigating
+distance = <value>;
+
+// Still investigating
+distanceZoomMax = <value>;
+
+// Still investigating
+distanceZoomMin = <value>;
+
+// Durability of the item before it breaks or degrades.
+durability = 100;
+
+// Still investigating
+emptyBody = <value>;
+
+// Still investigating
+emptyFeet = <value>;
+
+// Still investigating
+emptyGloves = <value>;
+
+// Still investigating
+emptyHead = <value>;
+
+// Still investigating
+emptyLegs = <value>;
+
+// Still investigating
+enabled = <value>;
+
+// Still investigating
+enabledPS4 = <value>;
+
+// Still investigating
+enabledXbox = <value>;
+
+// Still investigating
+energyUsagePerSecond = <value>;
+
+// Still investigating
+events[] = <value>;
+
+// Still investigating
+extCamTopZoomCoef = <value>;
+
+// Still investigating
+extCameraPositionMoving[] = <value>;
+
+// Still investigating
+extCameraPositionRaisedMoving[] = <value>;
+
+// Still investigating
+extCameraPositionRaisedWeaponMoving[] = <value>;
+
+// Still investigating
+extCameraPositionRaisedWeapon[] = <value>;
+
+// Still investigating
+extCameraPositionRaised[] = <value>;
+
+// Still investigating
+extCameraPosition[] = <value>;
+
+// Still investigating
+extCameraSpeedBound[] = <value>;
+
+// Still investigating
+eyeMaxAngle = <value>;
+
+// Still investigating
+eyelidDownMaxAngle = <value>;
+
+// Still investigating
+eyelidDownStartAngle = <value>;
+
+// Still investigating
+eyelidUpMaxAngle = <value>;
+
+// Still investigating
+eyelidUpStartAngle = <value>;
+
+// Still investigating
+face = <value>;
+
+// Still investigating
+faceType = <value>;
+
+// Faction or group the item belongs to.
+faction = "Civilian";
+
+// Still investigating
+fatalInjuryCoef = <value>;
+
+// Still investigating
+female = <value>;
+
+// Still investigating
+flareAngleBias = <value>;
+
+// Still investigating
+glasses = <value>;
+
+// Still investigating
+graphName = <value>;
+
+// Still investigating
+graphname = <value>;
+
+// Still investigating
+handGunBone = <value>;
+
+// Still investigating
+hasIcon = <value>;
+
+// Still investigating
+head = <value>;
+
+// Still investigating
+headSelectionsToHide[] = <value>;
+
+// Still investigating
+healthLabels[] = <value>;
+
+// Still investigating
+healthLevels[] = <value>;
+
+// Still investigating
+heatIsolation = <value>;
+
+// Still investigating
+hiddenSelectionsMaterials[] = <value>;
+
+// Still investigating
+hiddenSelectionsTextures[] = <value>;
+
+// Still investigating
+hiddenSelections[] = <value>;
+
+// Still investigating
+hideSelectionsByinventorySlot = <value>;
+
+// Still investigating
+hitpoint = <value>;
+
+// Still investigating
+hitpoints = <value>;
+
+// Still investigating
+htMax = <value>;
+
+// Still investigating
+htMin = <value>;
+
+// Still investigating
+humanBonePairs[] = <value>;
+
+// Still investigating
+id = <value>;
+
+// Still investigating
+idleSounds = <value>;
+
+// Still investigating
+ikSetup = <value>;
+
+// Still investigating
+initPhase = <value>;
+
+// Still investigating
+inventorySlot = <value>;
+
+// Still investigating
+inventorySlot[] = <value>;
+
+// Still investigating
+inventorySlotsCoefs[] = <value>;
+
+// Still investigating
+inventorySlots[] = <value>;
+
+// Still investigating
+isStrap = <value>;
+
+// Still investigating
+item = <value>;
+
+// Still investigating
+itemInfo[] = <value>;
+
+// Size of the item in inventory where {x,y} represent width and height respectively.
+itemSize[] = {2, 2};
+
+// Still investigating
+itemsCargoSize[] = <value>;
+
+// Still investigating
+languages[] = <value>;
+
+// Still investigating
+launcherBone = <value>;
+
+// Still investigating
+lookAtFilterSpeed = <value>;
+
+// Still investigating
+lookAtFilterTimeout = <value>;
+
+// Loot category the item belongs to.
+lootCategory = "Clothing";
+
+// Still investigating
+lootTag[] = <value>;
+
+// Still investigating
+mFact = <value>;
+
+// Still investigating
+male = <value>;
+
+// Still investigating
+mask[] = <value>;
+
+// Still investigating
+mat[] = <value>;
+
+// Still investigating
+mat_blood = <value>;
+
+// Still investigating
+mat_normal = <value>;
+
+// Still investigating
+material = <value>;
+
+// Still investigating
+max = <value>;
+
+// Still investigating
+meleeAmmo = <value>;
+
+// Still investigating
+memberSelections[] = <value>;
+
+// Still investigating
+memoryPointAim = <value>;
+
+// Still investigating
+memoryPointAimingAxis = <value>;
+
+// Still investigating
+memoryPointCamera = <value>;
+
+// Still investigating
+memoryPointCameraTarget = <value>;
+
+// Still investigating
+memoryPointCommonDamage = <value>;
+
+// Still investigating
+memoryPointHeadAxis = <value>;
+
+// Still investigating
+memoryPointLStep = <value>;
+
+// Still investigating
+memoryPointLeaningAxis = <value>;
+
+// Still investigating
+memoryPointRStep = <value>;
+
+// Still investigating
+meshObject = <value>;
+
+// Still investigating
+mfMax = <value>;
+
+// Still investigating
+min = <value>;
+
+// Still investigating
+minDistance = <value>;
+
+// Still investigating
+minScopeZoom = <value>;
+
+// Still investigating
+model = <value>;
+
+// Still investigating
+modelOptics = <value>;
+
+// Still investigating
+moveAnimNames[] = <value>;
+
+// Still investigating
+movementSpeedMapping[] = <value>;
+
+// Still investigating
+name = <value>;
+
+// Still investigating
+noEyewear = <value>;
+
+// Still investigating
+noMask = <value>;
+
+// Still investigating
+noNVStrap = <value>;
+
+// Still investigating
+noise = <value>;
+
+// Still investigating
+onlyInIronsight = <value>;
+
+// Indicates if the item can be opened or closed.
+openable = 0;
+
+// Still investigating
+opticsZoomInit = <value>;
+
+// Still investigating
+opticsZoomMax = <value>;
+
+// Still investigating
+opticsZoomMin = <value>;
+
+// Still investigating
+pitch = <value>;
+
+// Still investigating
+pitchAngle = <value>;
+
+// Still investigating
+playerSlots[] = <value>;
+
+// Still investigating
+plugType = <value>;
+
+// Still investigating
+position = <value>;
+
+// Still investigating
+protocol = <value>;
+
+// Still investigating
+quantityMinMaxCoef[] = <value>;
+
+// Still investigating
+quickBarBonus = <value>;
+
+// Still investigating
+radius = <value>;
+
+// Still investigating
+ragQuantity = <value>;
+
+// Still investigating
+randomQuantity = <value>;
+
+// Action range of the item, typically related to weapons.
+range = 100;
+
+// Still investigating
+rarityUrban = <value>;
+
+// Still investigating
+repairCosts[] = <value>;
+
+// Still investigating
+repairableWithKits[] = <value>;
+
+// Still investigating
+repeatable = <value>;
+
+// Still investigating
+requiredAddons[] = <value>;
+
+// Still investigating
+requiredVersion = <value>;
+
+// Still investigating
+reversed = <value>;
+
+// Still investigating
+rotationFlags = <value>;
+
+// Scope or range of the item, typically related to visibility or use.
+scope = 2;
+
+// Still investigating
+selection = <value>;
+
+// Still investigating
+selectionBodyWound = <value>;
+
+// Still investigating
+selectionEyelid = <value>;
+
+// Still investigating
+selectionHeadHide = <value>;
+
+// Still investigating
+selectionHeadWound = <value>;
+
+// Still investigating
+selectionLArmWound = <value>;
+
+// Still investigating
+selectionLBrow = <value>;
+
+// Still investigating
+selectionLLegWound = <value>;
+
+// Still investigating
+selectionLMouth = <value>;
+
+// Still investigating
+selectionLip = <value>;
+
+// Still investigating
+selectionMBrow = <value>;
+
+// Still investigating
+selectionMMouth = <value>;
+
+// Still investigating
+selectionNeckHide = <value>;
+
+// Still investigating
+selectionPersonality = <value>;
+
+// Still investigating
+selectionRArmWound = <value>;
+
+// Still investigating
+selectionRBrow = <value>;
+
+// Still investigating
+selectionRLegWound = <value>;
+
+// Still investigating
+selectionRMouth = <value>;
+
+// Still investigating
+simpleHiddenSelections[] = <value>;
+
+// Still investigating
+simpleSelectionName[] = <value>;
+
+// Type of simulation applied to the item in the game engine.
+simulation = "itemSim";
+
+// Still investigating
+skeletonName = <value>;
+
+// Still investigating
+slot = <value>;
+
+// Still investigating
+soundAttType = <value>;
+
+// Still investigating
+soundImpactType = <value>;
+
+// Still investigating
+soundLookupTable = <value>;
+
+// Still investigating
+soundSet = <value>;
+
+// Still investigating
+soundVoicePriority = <value>;
+
+// Still investigating
+soundVoiceType = <value>;
+
+// Still investigating
+soundset = <value>;
+
+// Still investigating
+source = <value>;
+
+// Still investigating
+speaker = <value>;
+
+// Still investigating
+speedMultiplier = <value>;
+
+// Still investigating
+spottedSounds = <value>;
+
+// Still investigating
+stanceName = <value>;
+
+// Still investigating
+startNode = <value>;
+
+// Still investigating
+startnode = <value>;
+
+// Still investigating
+storageCategory = <value>;
+
+// Still investigating
+strength = <value>;
+
+// Still investigating
+supplyRadius = <value>;
+
+// Still investigating
+tBody = <value>;
+
+// Still investigating
+teamName = <value>;
+
+// Still investigating
+tex[] = <value>;
+
+// Still investigating
+texture = <value>;
+
+// Still investigating
+threat[] = <value>;
+
+// Still investigating
+time = <value>;
+
+// Still investigating
+transferToAttachmentsCoef = <value>;
+
+// Still investigating
+transferToGlobalCoef = <value>;
+
+// Still investigating
+transferToZonesCoefs[] = <value>;
+
+// Still investigating
+transferToZonesNames[] = <value>;
+
+// General type or category of the item.
+type = "Clothing";
+
+// Still investigating
+units[] = <value>;
+
+// Still investigating
+updateInterval = <value>;
+
+// Still investigating
+useCursor = <value>;
+
+// Still investigating
+varWetMax = <value>;
+
+// Still investigating
+variants[] = <value>;
+
+// Still investigating
+vehicleClass = <value>;
+
+// Modifier affecting the item's visibility in the game.
+visibilityModifier = 1.0;
+
+// Still investigating
+vizem[] = <value>;
+
+// Still investigating
+voiceType = <value>;
+
+// Still investigating
+weaponBone = <value>;
+
+// Still investigating
+weapons[] = <value>;
+
+// Weight of the item.
+weight = 0.3;
+
+// Still investigating
+woman = <value>;
+
+// Still investigating
+yawAngle = <value>;
+
+// Still investigating
+zombieLoot = <value>;
